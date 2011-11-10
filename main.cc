69a49b8ea7d2b1e9828b55dc0f3de7aa56874f79
Miguel Diaz <good_mourning@hotmail.com>
2011-11-09 18:42:12 -0600

    Added appropriate assertions for block size, associativity, and capacity

--- main.cc
+++ main.cc
@@ -3,6 +3,7 @@ Ian Buitrago
 Miguel Diaz
 */
 
+#include <cassert>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
@@ -24,7 +25,22 @@ int main (int argc, char *argv[ ])
   if(!parseParams(argc, argv, cache_capacity, cache_blocksize, cache_associativity)) {
 	exit(2);
   }
-
+  
+  // -c <capacity> with <capacity> in KB: 4, 8, 16, 32, or 64. 
+  assert(cache_capacity == 4 || cache_capacity == 8 || cache_capacity == 16
+         || cache_capacity == 32 || cache_capacity == 64); 
+  // -b <blocksize> with <blocksize> in bytes:
+  //  4, 8, 16, 32, 64, 128, 256, or 512. 
+  assert(cache_blocksize == 4 || cache_blocksize == 8 || cache_blocksize == 16
+         || cache_blocksize == 32 || cache_blocksize == 64 
+         || cache_blocksize == 128 || cache_blocksize == 256
+         || cache_blocksize == 512); 
+  // -a <associativity> where <associativity> is integer size of set:
+  // 1, 2, 4, 8, 16. 
+  assert(cache_associativity == 1 || cache_associativity == 2
+         || cache_associativity == 4 || cache_associativity == 8
+         || cache_associativity == 16);
+  
   cout << "Cache Capacity: " << cache_capacity << endl;
   cout << "Cache BlockSize: " << cache_blocksize << endl;
   cout << "Cache Associativity: " << cache_associativity << endl;
