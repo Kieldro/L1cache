53692e36456cfbd07d617488dbc476a66afd9f49
Miguel Diaz <good_mourning@hotmail.com>
2011-11-16 15:48:28 -0600

    rough sketch of cache_memory.h -- plus minus one or two variables/functions, should be good

---
+++ cache_memory.h
@@ -0,0 +1,30 @@
+#ifndef _CACHE_MEMORY_H
+#define _CACHE_MEMORY_H
+
+#define HEX 0
+#define DEC 1
+#define OCT 2
+
+#include<iostream>
+
+using namespace std;
+
+class CacheMemory{
+	private:
+		int *memory;
+		bool *valid;
+		bool *dirty;
+		int *tag;
+		int *data;
+		int size;
+		int associativity;
+	public: 
+		CacheMemory(int capacity);
+		~CacheMemory();
+		void print_contents(int from, int to, int format);
+		void set_content(int location, int value);
+		void reset_content(void);
+};
+
+#endif
+
